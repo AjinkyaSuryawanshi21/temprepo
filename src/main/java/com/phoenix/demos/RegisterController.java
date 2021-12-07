1c3f4b51d386076bdafbb884d7f0be361cf52591
Ajinkya Suryawanshi <ajinkya.s@DM1370.datametica.com>
2021-12-07 16:15:11 +0530

    First

---
+++ src/main/java/com/phoenix/demos/RegisterController.java
@@ -0,0 +1,16 @@
+package com.phoenix.demos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class RegisterController{
+    @RequestMapping("/register.spring")
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView ref = new ModelAndView("next");
+        return ref;
+    }
+}
